7e3ed9d1c8c7f84b43e7663916fa63ec6da95598
zaneliu <hi@zaneliu.me>
2026-02-06 00:06:54 +0800

    feat: add VariableSuggestion extension with {{ trigger

---
+++ web/src/components/workflow/smart-input/extensions/index.ts
@@ -0,0 +1,7 @@
+export { VariableNode } from './VariableNode'
+export {
+ createVariableSuggestion,
+ variableSuggestionPluginKey,
+ type VariableSuggestionOptions,
+ type SuggestionProps,
+} from './VariableSuggestion'
