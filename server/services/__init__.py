7e0a9e19e1d1d1ac0fe50a17c4c06bfabb5fa313
zaneliu <hi@zaneliu.me>
2026-02-14 12:18:36 +0800

    feat: export cache managers from services package

--- server/services/__init__.py
+++ server/services/__init__.py
@@ -1,1 +1,13 @@
 """Services package."""
+from services.dependency_cache import (
+ DependencyCacheManager,
+ LockFileInfo,
+ PackageManager,
+)
+from services.repo_cache_manager import RepoCacheManager
+__all__ = [
+ "DependencyCacheManager",
+ "LockFileInfo",
+ "PackageManager",
+ "RepoCacheManager",
+]
