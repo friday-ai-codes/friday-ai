17a953f95fe56e325a31dd2de2692d5f2527a242
zaneliu <hi@zaneliu.me>
2026-02-20 02:25:35 +0800

    feat: MonitorConsumer + 路由注册

--- server/runners/routing.py
+++ server/runners/routing.py
@@ -1,7 +1,8 @@
 """Runner WebSocket URL 路由。"""
 from django.urls import re_path
-from .consumers import RunnerConsumer
+from .consumers import MonitorConsumer, RunnerConsumer
 from .middleware import RunnerTokenAuthMiddleware
 websocket_urlpatterns = [
  re_path(r"ws/v1/runner/$", RunnerTokenAuthMiddleware(RunnerConsumer.as_asgi)),
+ re_path(r"ws/v1/runners/monitor/$", MonitorConsumer.as_asgi),
 ]
