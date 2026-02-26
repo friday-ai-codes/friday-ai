f35bacad0a0f1f8b00e30e82ee26cfe27f307488
zaneliu <hi@zaneliu.me>
2026-02-26 19:24:01 +0800

    feat: create IntegrationNode.vue for integration category nodes

---
+++ web/src/components/workflow/editor/nodes/IntegrationNode.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { FileText, FolderSearch, Plug } from 'lucide-vue-next'
+import { computed, type Component } from 'vue'
+import BaseWorkflowNode from './BaseWorkflowNode.vue'
+const props = defineProps<{
+ id: string
+ data: {
+ name: string
+ nodeType: string
+ description?: string
+ disabled?: boolean
+ [key: string]: unknown
+ }
+ selected?: boolean
+}>
+const iconMap: Record<string, Component> = {
+ fetch_work_item: FileText,
+ fetch_project_info: FolderSearch,
+}
+const icon = computed( => iconMap[props.data.nodeType] ?? Plug)
+</script>
+<template>
+ <BaseWorkflowNode:id="id":data="data":selected="selected">
+ <template #icon>
+ <component:is="icon" class="w-4 " />
+ </template>
+ <template v-if="data.description" #content>
+ <p class="text-xs text-muted-foreground">{{ data.description }}</p>
+ </template>
+ </BaseWorkflowNode>
+</template>
