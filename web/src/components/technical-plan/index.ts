838cb404a23c12a3eeeca4e7d39df69d5dc26546
zaneliu <hi@zaneliu.me>
2026-03-02 01:59:04 +0800

    feat: delete TechnicalPlanConfig.vue and technical-plan directory

--- web/src/components/technical-plan/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TechnicalPlanEditor } from './TechnicalPlanEditor.vue'
-export { default as TechnicalPlanPanel } from './TechnicalPlanPanel.vue'
-export { default as TechnicalPlanViewer } from './TechnicalPlanViewer.vue'
