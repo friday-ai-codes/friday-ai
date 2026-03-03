481daa071bab0e7830c6e6ff4b85cc58ee17e0c8
zaneliu <hi@zaneliu.me>
2026-03-03 16:26:19 +0800

    feat: 添加 shadcn-vue Sheet 组件

---
+++ web/src/components/ui/sheet/SheetTitle.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { DialogTitleProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { DialogTitle } from 'reka-ui'
+import { cn } from '~/lib/utils'
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes['class'] }>
+</script>
+<template>
+ <DialogTitle:class="cn('text-lg font-semibold text-foreground', props.class)">
+ <slot />
+ </DialogTitle>
+</template>
