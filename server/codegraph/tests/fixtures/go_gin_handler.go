bafb33879c6cacba8b687230b132272d80fcf63d
zaneliu <hi@zaneliu.me>
2026-05-15 21:21:31 +0800

    test: 新增 go_gin_handler.go fixture（GET + POST handler）

---
+++ server/codegraph/tests/fixtures/go_gin_handler.go
@@ -0,0 +1,25 @@
+package handler
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+type User struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+}
+func GetUser(c *gin.Context) {
+	id:= c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+func CreateUser(c *gin.Context) {
+	var u User
+	if err:= c.BindJSON(&u); err != nil {
+ c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+ return
+	}
+	c.JSON(http.StatusCreated, u)
+}
+func RegisterRoutes(r *gin.Engine) {
+	r.GET("/users/:id", GetUser)
+	r.POST("/users", CreateUser)
+}
