bed9a80a5a97adeced86089d584fecf49f2d3bcd
zaneliu <hi@zaneliu.me>
2026-05-15 23:13:46 +0800

    test: 新增 vue27_setup.vue fixture（Vue 2.7 script setup TS 范例）

---
+++ server/codegraph/tests/fixtures/vue27_setup.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+ <p>{{ message }}</p>
+ <button @click="onClick">点我</button>
+ </div>
+</template>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { fetchData } from './api'
+interface Props {
+ initialMessage: string
+}
+const props = defineProps<Props>
+const message = ref(props.initialMessage)
+const length = computed( => message.value.length)
+function onClick {
+ fetchData.then((data) => {
+ message.value = data
+ })
+}
+const reset = => {
+ message.value = ''
+}
+</script>
+<style scoped>
+button { color: red; }
+</style>
