b87bae1feb7097d0a60dabb9df6a96d98583eb34
zaneliu <hi@zaneliu.me>
2026-05-29 20:46:47 +0800

    test: 子组件 TEMPLATE_REF 与 TSX JSX 口径单测

---
+++ server/codegraph/tests/fixtures/vue_with_children.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="wrapper">
+ <ChildComp:title="title" />
+ <user-card @save="onSave">{{ title }}</user-card>
+ <span>{{ title }}</span>
+ </div>
+</template>
+<script setup lang="ts">
+import ChildComp from './ChildComp.vue'
+import UserCard from './UserCard.vue'
+const title = 'hello'
+function onSave {
+ return title
+}
+</script>
+<style scoped>
+.wrapper { display: flex; }
+</style>
